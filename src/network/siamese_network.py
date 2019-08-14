157be5898287551aa6f6c6f324bbb9f71291df29
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-08-14 12:41:33 +0530

    Fix Merge Conflicts

--- src/network/siamese_network.py
+++ src/network/siamese_network.py
@@ -1,45 +1,43 @@
 import torch.nn as nn
 
+import torch
+from vgg import *
+from resnet import *
+from alexnet import *
+from squeezenet import *
+from densenet import *
+from inceptionv3 import *
+from googlenet import *
+
+models = {
+    'alexnet': AlexNet,
+    'vgg11': vgg11,
+    'vgg13': vgg13,
+    'vgg16': vgg16,
+    'vgg19': vgg19,
+    'sqnet1_0': SqueezeNet1_0,
+    'sqnet1_1': SqueezeNet1_1,
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+    'resnet101': resnet101,
+    'resnet152': resnet152,
+    'densenet121': densenet121,
+    'densenet161': densenet161,
+    'densenet169': densenet169,
+    'densenet201': densenet201,
+    'googlenet': googlenet,
+    'inception': inception_v3
+}
+
 
 class SiameseNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
         super(SiameseNetwork, self).__init__()
-        self.cnn1 = nn.Sequential(
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(1, 4, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(4),
-
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(4, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
-
-
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(8, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
-
-
-        )
-
-        self.fc1 = nn.Sequential(
-            nn.Linear(8*100*100, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 5))
-
-    def forward_once(self, x):
-        output = self.cnn1(x)
-        output = output.view(output.size()[0], -1)
-        output = self.fc1(output)
-        return output
+        self.network = models[config.arch]()
 
     def forward(self, input1, input2):
-        output1 = self.forward_once(input1)
-        output2 = self.forward_once(input2)
+        output1 = self.network(input1)
+        output2 = self.network(input2)
+        print(output1.shape, output2.shape)
         return output1, output2
--- src/train.py
+++ src/train.py
@@ -18,50 +18,65 @@ from loss.squared_euclidean_loss import EucledianLoss
 from network.siamese_network import SiameseNetwork
 from dataset.siamese_network_dataset import SiameseNetworkDataset
 
-folder_dataset = dset.ImageFolder(root=Config.training_dir)
-rdm = get_rdm()
-siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset, rdm=rdm,
-                                        transform=transforms.Compose([transforms.Resize((100, 100)),
-                                                                      transforms.ToTensor()
-                                                                      ]), should_invert=False)
 
-vis_dataloader = DataLoader(siamese_dataset,
-                            shuffle=True,
-                            num_workers=8,
-                            batch_size=8)
-dataiter = iter(vis_dataloader)
+def prepare_dataset(config):
+    folder_dataset = dset.ImageFolder(root=config.train_dir)
+    rdm = get_rdm()
+    siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset, rdm=rdm,
+                                            transform=transforms.Compose([transforms.Resize((100, 100)),
+                                                                          transforms.ToTensor()
+                                                                          ]), should_invert=False)
+
+    vis_dataloader = DataLoader(siamese_dataset,
+                                shuffle=True,
+                                num_workers=8,
+                                batch_size=8)
+    dataiter = iter(vis_dataloader)
+
+    example_batch = next(dataiter)
+    concatenated = torch.cat((example_batch[0], example_batch[1]), 0)
+    train_dataloader = DataLoader(siamese_dataset,
+                                  shuffle=True,
+                                  num_workers=8,
+                                  batch_size=config.batch_size)
+    return train_dataloader
 
 
-example_batch = next(dataiter)
-concatenated = torch.cat((example_batch[0], example_batch[1]), 0)
-imshow(torchvision.utils.make_grid(concatenated))
-train_dataloader = DataLoader(siamese_dataset,
-                              shuffle=True,
-                              num_workers=8,
-                              batch_size=Config.train_batch_size)
+def train(train_dataloader, config):
+    net = SiameseNetwork(config).cuda()
+    print(net)
+    criterion = EucledianLoss()
+    # criterion = ContrastiveLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0005)
 
-net = SiameseNetwork().cuda()
-criterion = EucledianLoss()
-# criterion = ContrastiveLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.0005)
+    counter = []
+    loss_history = []
+    iteration_number = 0
 
-counter = []
-loss_history = []
-iteration_number = 0
+    for epoch in range(0, config.num_epochs):
+        for i, data in enumerate(train_dataloader, 0):
+            img0, img1, label = data
+            img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
+            optimizer.zero_grad()
+            output1, output2 = net(img0, img1)
+            loss_contrastive = criterion(output1, output2, label)
+            loss_contrastive.backward()
+            optimizer.step()
+            if i % 10 == 0:
+                print("Epoch number {}\n Current loss {}\n".format(
+                    epoch, loss_contrastive.item()))
+                iteration_number += 10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.item())
+    show_plot(counter, loss_history)
 
-for epoch in range(0, Config.train_number_epochs):
-    for i, data in enumerate(train_dataloader, 0):
-        img0, img1, label = data
-        img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
-        optimizer.zero_grad()
-        output1, output2 = net(img0, img1)
-        loss_contrastive = criterion(output1, output2, label)
-        loss_contrastive.backward()
-        optimizer.step()
-        if i % 10 == 0:
-            print("Epoch number {}\n Current loss {}\n".format(
-                epoch, loss_contrastive.item()))
-            iteration_number += 10
-            counter.append(iteration_number)
-            loss_history.append(loss_contrastive.item())
-show_plot(counter, loss_history)
+
+def main(config):
+    train_dataloader = prepare_dataset(config)
+    train(train_dataloader, config)
+
+
+if __name__ == '__main__':
+    config = Config().parse()
+    print(config)
+    main(config)
