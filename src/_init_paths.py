da8dea7046123518ad7dd134a8c95d8d5a7e93b5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-08-17 15:24:57 +0530

    Add train to PYTHONPATH

--- src/_init_paths.py
+++ src/_init_paths.py
@@ -1,14 +1,17 @@
 import os
 import sys
 
+
 def add_path(path):
     if path not in sys.path:
         sys.path.insert(0, path)
+
 
 this_dir = os.path.dirname(__file__)
 # Add loss to PYTHONPATH
 lib_path = os.path.join(this_dir, 'loss')
 add_path(lib_path)
+
 # Add utils to PYTHONPATH
 lib_path = os.path.join(this_dir, 'utils')
 add_path(lib_path)
@@ -19,4 +22,8 @@ add_path(lib_path)
 
 # Add dataset to PYTHONPATH
 lib_path = os.path.join(this_dir, 'dataset')
+add_path(lib_path)
+
+# Add train to PYTHONPATH
+lib_path = os.path.join(this_dir, 'train')
 add_path(lib_path)
