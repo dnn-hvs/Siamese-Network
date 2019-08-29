2075ffae764d3c30850c9662821578c41384966d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-08-29 20:15:15 +0530

    Add skeleton foveation script

---
+++ src/prepare_foveated_dataset.py
@@ -0,0 +1,34 @@
+from itertools import combinations
+import os
+from shutil import copy
+from .utils import foveate
+
+try:
+    os.mkdir('../data_foveated')
+    os.mkdir('../data_foveated/92_images')
+    os.mkdir('../data_foveated/118_images')
+
+except FileExistsError:
+    pass
+image_92_path = '../data/Training_Data/92_Image_Set/92images/'
+image_118_path = '../data/Training_Data/118_Image_Set/118images/'
+images_92 = os.listdir(image_92_path)
+images_118 = os.listdir(image_118_path)
+print(len(images_118), len(images_92))
+
+combs_92 = list(combinations(images_92, 2))
+combs_118 = list(combinations(images_118, 2))
+tot_list = combs_92 + combs_118
+
+for i, val in enumerate(combs_92):
+    new_dir = '../data_foveated/92_images/' + str(i)
+    os.mkdir(new_dir)
+    copy(image_92_path+val[0], new_dir)
+    copy(image_92_path+val[1], new_dir)
+
+
+for i, val in enumerate(combs_118):
+    new_dir = '../data_foveated/118_images/' + str(i)
+    os.mkdir(new_dir)
+    copy(image_118_path+val[0], new_dir)
+    copy(image_118_path+val[1], new_dir)
