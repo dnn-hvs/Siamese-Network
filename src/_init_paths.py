9c05ea52c4e98dd4d245a1e16f7ff55fc613d309
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-12-01 12:16:15 +0530

    Remove unecessary comments and add algonauts to the path

--- src/_init_paths.py
+++ src/_init_paths.py
@@ -11,21 +11,5 @@ this_dir = os.path.dirname(__file__)
 # Add loss to PYTHONPATH
 lib_path = os.path.join(this_dir, 'lib')
 add_path(lib_path)
-# lib_path = os.path.join(this_dir, 'loss')
-# add_path(lib_path)
-
-# # Add utils to PYTHONPATH
-# lib_path = os.path.join(this_dir, 'utils')
-# add_path(lib_path)
-
-# # Add network to PYTHONPATH
-# lib_path = os.path.join(this_dir, 'network')
-# add_path(lib_path)
-
-# # Add dataset to PYTHONPATH
-# lib_path = os.path.join(this_dir, 'dataset')
-# add_path(lib_path)
-
-# # Add train to PYTHONPATH
-# lib_path = os.path.join(this_dir, 'train')
-# add_path(lib_path)
+lib_path = os.path.join(this_dir, 'algonauts')
+add_path(lib_path)
